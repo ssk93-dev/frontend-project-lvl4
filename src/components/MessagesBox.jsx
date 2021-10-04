56e059655e63882f4056864aa3037d6e6aea7101
Sergei Kalikin <sskalikin@gmail.com>
2021-10-04 22:52:06 +0500

    fix: moved message form to messages box

--- src/components/MessagesBox.jsx
+++ src/components/MessagesBox.jsx
@@ -1,8 +1,41 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useEffect, useRef, useContext } from 'react';
 import { useSelector } from 'react-redux';
+import {
+  Form, InputGroup, Button,
+} from 'react-bootstrap';
+import { useFormik } from 'formik';
 import _ from 'lodash';
 import { useTranslation } from 'react-i18next';
-import MessageForm from './MessageForm.jsx';
+import Context from '../context.jsx';
+
+const MessageForm = () => {
+  const { currentChannelId } = useSelector((state) => state);
+  const { globalState } = useContext(Context);
+  const { user, socket } = globalState;
+  const { t } = useTranslation();
+  const inputRef = useRef();
+  const formik = useFormik({
+    initialValues: {
+      text: '',
+    },
+    onSubmit: (values) => {
+      socket.emit('newMessage', { username: user.username, channelId: currentChannelId, body: values.text });
+      formik.resetForm();
+    },
+  });
+  useEffect(() => {
+    inputRef.current.focus();
+  });
+  return (
+    <Form onSubmit={formik.handleSubmit}>
+      <InputGroup>
+        <Form.Label visuallyHidden>{t('messages.input')}</Form.Label>
+        <Form.Control ref={inputRef} name="text" type="text" placeholder={t('messages.input')} onChange={formik.handleChange} value={formik.values.text} />
+        <Button type="submit" variant="outline-secondary">{t('messages.send')}</Button>
+      </InputGroup>
+    </Form>
+  );
+};
 
 const MessagesBox = () => {
   const { channels, currentChannelId, messages } = useSelector((state) => state);
