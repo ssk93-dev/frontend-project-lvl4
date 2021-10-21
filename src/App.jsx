de9439f2f356a0e88c90707fd4076f61a4facc81
Sergei Kalikin <sskalikin@gmail.com>
2021-10-21 23:40:44 +0500

    fix: toast handler

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ const App = ({ socket }) => {
   const dispatch = useDispatch();
   const toastState = useSelector(getToastState);
   const modalInfo = useSelector(getModalState);
-  const handleToast = (payload) => () => dispatch(actions.handleToast(payload));
+  const hideToast = () => dispatch(actions.handleToast({ toastState: { show: false } }));
   const initialState = {
     user: { username, token },
     isLoggedIn: !!token,
@@ -47,7 +47,7 @@ const App = ({ socket }) => {
     >
       <Header />
       <ToastContainer position="top-center">
-        <Toast bg="danger" show={toastState.show} onClose={handleToast({ toastState: { show: false } })}>
+        <Toast bg="danger" show={toastState.show} onClose={hideToast}>
           <Toast.Header>
             <strong className="me-auto">{t('errors.network')}</strong>
           </Toast.Header>
