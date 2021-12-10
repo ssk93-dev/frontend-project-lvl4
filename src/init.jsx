6f8218bc636f0e1544a012739ea3fb1b77241b2a
Sergei Kalikin <sskalikin@gmail.com>
2021-12-11 00:19:30 +0500

    fix: rollbar

--- src/init.jsx
+++ src/init.jsx
@@ -4,10 +4,16 @@ import { I18nextProvider, initReactI18next } from 'react-i18next';
 import { toast } from 'react-toastify';
 import { Spinner } from 'react-bootstrap';
 import { Provider } from 'react-redux';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 import store from './store/index.js';
 import App from './App.jsx';
 import resources from './locales/index.js';
 import getApi from './Api.jsx';
+
+const rollbarConfig = {
+  accessToken: 'eb8cd7f6400946319cb3441f0e28dd68',
+  environment: 'production',
+};
 
 const init = async (socket) => {
   const { ApiProvider, startConnectListener, startDisconnectListener } = getApi(socket);
@@ -26,13 +32,17 @@ const init = async (socket) => {
   startConnectListener(() => toast.dismiss('connection'));
 
   return (
-    <Provider store={store}>
-      <I18nextProvider i18n={i18nInstance}>
-        <ApiProvider>
-          <App />
-        </ApiProvider>
-      </I18nextProvider>
-    </Provider>
+    <RollbarProvider config={rollbarConfig}>
+      <ErrorBoundary>
+        <Provider store={store}>
+          <I18nextProvider i18n={i18nInstance}>
+            <ApiProvider>
+              <App />
+            </ApiProvider>
+          </I18nextProvider>
+        </Provider>
+      </ErrorBoundary>
+    </RollbarProvider>
   );
 };
 
