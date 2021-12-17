e3a3df208a650e18bd9a41a9876d2fbd8bb68cef
Sergei Kalikin <sskalikin@gmail.com>
2021-12-18 00:12:30 +0500

    fix: removed useless fragment form App.js

--- src/App.jsx
+++ src/App.jsx
@@ -20,47 +20,45 @@ const App = () => {
   const modalInfo = useSelector(getModalState);
 
   return (
-    <>
-      <AuthApi>
-        <Col className="d-flex flex-column h-100 bg-light" aria-hidden={modalInfo.showed}>
-          <Router>
-            <Header />
-            <Switch>
-              <Route exact path="/">
-                <PrivateRoute path="/login">
-                  <ChatPage />
-                </PrivateRoute>
-              </Route>
-              <Route exact path="/login">
-                <PublicRoute path="/">
-                  <LoginPage />
-                </PublicRoute>
-              </Route>
-              <Route exact path="/signup">
-                <PublicRoute path="/">
-                  <SignupPage />
-                </PublicRoute>
-              </Route>
-              <Route path="*">
-                <NotFound />
-              </Route>
-            </Switch>
-          </Router>
-        </Col>
-        <MyModal />
-        <ToastContainer
-          position="top-center"
-          autoClose={3000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-        />
-      </AuthApi>
-    </>
+    <AuthApi>
+      <Col className="d-flex flex-column h-100 bg-light" aria-hidden={modalInfo.showed}>
+        <Router>
+          <Header />
+          <Switch>
+            <Route exact path="/">
+              <PrivateRoute path="/login">
+                <ChatPage />
+              </PrivateRoute>
+            </Route>
+            <Route exact path="/login">
+              <PublicRoute path="/">
+                <LoginPage />
+              </PublicRoute>
+            </Route>
+            <Route exact path="/signup">
+              <PublicRoute path="/">
+                <SignupPage />
+              </PublicRoute>
+            </Route>
+            <Route path="*">
+              <NotFound />
+            </Route>
+          </Switch>
+        </Router>
+      </Col>
+      <MyModal />
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </AuthApi>
   );
 };
 
