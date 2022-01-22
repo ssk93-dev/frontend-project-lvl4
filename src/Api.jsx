f34a7c05753807181d21287cc01610e55f2ae14e
Sergei Kalikin <sskalikin@gmail.com>
2022-01-22 23:38:36 +0500

    fix: delay

--- src/Api.jsx
+++ src/Api.jsx
@@ -5,15 +5,12 @@ import { msgActions } from './store/slices/messagesSlice.js';
 
 const getApi = (socket, store) => {
   const errorMessage = 'errors.network';
-<<<<<<< HEAD
-  const REJECT_DELAY = 3000;
-=======
->>>>>>> parent of 19f9563... fix: reject delay
+  const REJECT_DELAY = 5000;
 
   const promisifySocket = (socketFunction) => (...payload) => new Promise((resolve, reject) => {
     const timer = setTimeout(() => {
       reject(errorMessage);
-    }, 3000);
+    }, REJECT_DELAY);
     socketFunction(...payload, ({ status }) => {
       clearTimeout(timer);
       if (status === 'ok') {
